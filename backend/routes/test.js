7a1f2a9b19496340f6ff2297ed6eca1a8dadc6c5
Lakmana Thabrew <chulankalakmana@gmail.com>
2026-01-18 00:31:02 +0530

    Fix errors

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import User from './models/user.js';
+import User from '../models/user.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
